36fe1c10e0d379749e849a7101de7efd3591d8ba
susmit31 <gohanmysticdbz@gmail.com>
2021-12-04 23:29:00 +0600

    1610A solution v2

--- c/1610A_cell-guessing.cpp
+++ c/1610A_cell-guessing.cpp
@@ -2,16 +2,24 @@
 #include <cstdio>
 #include <vector>
 #include <string>
+#include <sstream>
 
 int min_points(int, int);
+void parse_input(std::string, int*);
 
 int main(){
     int N_INPS;
     std::cin >> N_INPS;
     int width, height;
     std::vector<std::vector<int>> inputs;
+    std::string inp_line;
+    getline(std::cin, inp_line);
+    int parsed[2];
     for (int i=0; i< N_INPS; i++){
-        scanf("%d %d", &width, &height);
+        getline(std::cin, inp_line);
+        parse_input(inp_line, parsed);
+        width = parsed[0];
+        height = parsed[1];
         inputs.push_back(std::vector<int> {width, height});
     }
     for (std::vector<int> dimensions: inputs){
@@ -26,4 +34,13 @@ int min_points(int width, int height){
     if (width==height && height==1) return 0;
     else if (height==1) return 1;
     else return 2;
+}
+
+void parse_input(std::string inp_str, int* output){
+    std::stringstream ss(inp_str);
+    int num, i=0;
+    while (ss >> num){
+        output[i] = num;
+        i++;
+    }
 }
